2b411b78c8997b35a218879f7be02d80527a6677
Marc <phyrenight@gmail.com>
2016-04-09 13:01:14 -0400

    fixed javascript

--- javascript/addChat.js
+++ javascript/addChat.js
@@ -1,10 +1,11 @@
-function postIt(chat){
-  //var node = document.createElement("div");
-  //var chat = document.getElementById('chitChat').value; 
-chatText = document.createTextNode(chat);
-console.log(chatText);
-node.appendChild(chatText);
+function postIt(){
+  var node = document.createElement("div");
+  var chat = document.getElementById('chitChat').value; 
+  var place = document.getElementById('talk')
+  chatText = document.createTextNode(chat);
+//place.innerHTML = chat.value;
+  node.appendChild(chatText);
 console.log("hi");
-post = document.getElementById('talk').appendChild(node);
-post.innerHTMl = chat;
+  post = document.getElementById('talk').appendChild(node);
+  post.innerHTMl = chat;
 }
